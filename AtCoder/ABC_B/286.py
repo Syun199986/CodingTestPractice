b69770e1a4adf37946e38e84f853cf52ec36eab2
Syun199986 <syun990806@gmail.com>
2023-01-21 23:51:54 +0900

    AtCoder 286_B before fix

---
+++ AtCoder/ABC_B/286.py
@@ -0,0 +1,18 @@
+n = int(input())
+s = input()
+ans_list = []
+i = 1
+
+while i < n:
+	if s[i-1] == "n" and s[i] == "a":
+		ans_list.append("nya")
+		i += 1
+	else:
+		ans_list.append(s[i-1])
+	i += 1
+
+ans_list.append(s[n-1])
+
+ans = ''.join(ans_list)
+
+print(ans)
